20bdf9c4e4a525d4d131743e2d3b9cfe363c6e29
AmbrozynePlay <ambrozyneplay@gmail.com>
2019-11-14 16:34:43 -0600

    Master

---
+++ header.php
@@ -0,0 +1,54 @@
+<?php
+<!DOCTYPE html> 
+<html lang="es"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- Materialize CSS -->
+    <link rel="stylesheet" href="css/materialize.css">
+<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.css">
+<!-- Style -->
+    <link rel="stylesheet" href="css/header.css">
+<!-- @media 576 CSS-->
+    <link rel="stylesheet" href="css/@media/index@media 576.css">
+<!-- @media 1024 CSS-->
+    <link rel="stylesheet" href="css/@media/index@media 1024.css">
+<!-- @media 1360 CSS-->
+    <link rel="stylesheet" href="css/@media/index@media 1360.css">
+<!-- Import Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet"> 
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/ef2fe0d0d7.js"></script>
+</head>
+<body>	
+<!-- Header -->
+	<header> 
+	<nav class="navbar navbar-expand-lg navbar-light">
+	<img id="navlogo" src="image/png/00.png" alt="00.png"/>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarcollapse" aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarcollapse">
+	<div class="navbar-nav">
+	<a class="nav-item nav-link active" href="#" id="n01">Home<span class="sr-only">(current)</span></a>
+	<a class="nav-item nav-link" href="#" id="n02">Modulo I</a>
+	<a class="nav-item nav-link" href="#" id="n03">Modulo II</a> 
+	<a class="nav-item nav-link" href="#" id="n04">Modulo III</a>
+	</div>
+	</div>
+	</nav>
+	</header>
+    
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.js"></script>
+    <!-- Compiled and minified JavaScript -->
+    <script src="js/materialize.js"></script>
+</body>
+</html>
+?>
